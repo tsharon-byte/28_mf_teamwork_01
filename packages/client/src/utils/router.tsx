a78db385de404e0e13ca777d66c84ad87eadeede
Avanire <drtony@yandex.ru>
2023-07-24 18:16:38 +0600

    Страница логина

--- packages/client/src/utils/router.tsx
+++ packages/client/src/utils/router.tsx
@@ -9,7 +9,7 @@ import Forum from '../pages/forum'
 import ForumPost from '../pages/forum-post'
 import EndGame from '../pages/end-game'
 import { ROUTE_PATH } from './constants'
-import ErrorPage from '../pages/error-page'  
+import ErrorPage from '../pages/error-page'
 
 const router = createBrowserRouter([
   {
