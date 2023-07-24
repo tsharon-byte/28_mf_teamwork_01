23a4e8b7107d5a1ff375a98f490170bdd641a65f
Sergey Kalachev <rollstream@yandex.ru>
2023-07-24 15:56:06 +0300

    fontsource import added

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './components/app'
+import '@fontsource/khand/400.css'
+import '@fontsource/notable/400.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
