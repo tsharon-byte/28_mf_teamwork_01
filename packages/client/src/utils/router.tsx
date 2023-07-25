27dd14badf7ce2df8a1d3687bc95a89bf969f459
Avanire <drtony@yandex.ru>
2023-07-25 19:15:08 +0600

    Добавлены защищенные роуты для зарегистрированных пользователей

--- packages/client/src/utils/router.tsx
+++ packages/client/src/utils/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import Home from '../pages/home'
 import Login from '../pages/login'
 import Registration from '../pages/registration'
