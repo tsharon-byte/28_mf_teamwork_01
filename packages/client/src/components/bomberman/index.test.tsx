b835f95327f037807461db64f38beba005fc1363
Ivan Axenenko <ioaksenenko@gmail.com>
2023-08-11 10:47:15 +0700

    added tests for bomberman component and animation helpers

---
+++ packages/client/src/components/bomberman/index.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Bomberman from './index'
+
+describe('test bomberman component', () => {
+  beforeEach(() => {
+    render(<Bomberman />)
+  })
+
+  test('should has canvas element', async () => {
+    expect(screen.getByTestId('canvas')).toBeInTheDocument()
+  })
+
+  test('should has start game button', () => {
+    expect(screen.getByText('Начать Игру')).toBeInTheDocument()
+  })
+
+  test('should has end game button', () => {
+    expect(screen.getByText('Окончить Игру')).toBeInTheDocument()
+  })
+})
