512c1211fc8e63d854bbe51058c4bda97386a6b2
Avanire <drtony@yandex.ru>
2023-08-16 17:04:01 +0600

    Удалил не используемый код

--- packages/client/src/components/app/index.tsx
+++ packages/client/src/components/app/index.tsx
@@ -6,17 +6,6 @@ import router from '../../utils/router'
 import { darkTheme } from '../../themes'
 
 const App: FC = () => {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
-
-    fetchServerData()
-  }, [])
-
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
