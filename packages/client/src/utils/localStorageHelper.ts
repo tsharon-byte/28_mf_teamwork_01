d8d0569b1bd39ae85125ff95ca30b35b13c5fcad
Tatiana Churkina <sharonova_t@inbox.ru>
2023-08-17 10:48:35 +0300

    Убрала шифрование перед демо

--- packages/client/src/utils/localStorageHelper.ts
+++ packages/client/src/utils/localStorageHelper.ts
@@ -1,13 +1,13 @@
 export const getFromLocalStorage = (name: string) => {
   const valueFromLocalStorage = localStorage.getItem(name)
   if (valueFromLocalStorage) {
-    return JSON.parse(atob(valueFromLocalStorage))
+    return JSON.parse(valueFromLocalStorage)
   }
   return null
 }
 
 export const setToLocalStorage = (name: string, value: any) => {
-  localStorage.setItem(name, btoa(JSON.stringify(value)))
+  localStorage.setItem(name, JSON.stringify(value))
 }
 
 export const deleteFromLocalStorage = (name: string) => {
