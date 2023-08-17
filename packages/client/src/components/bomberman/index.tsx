1452fc461da527d9c781812b3b9562b48b62a40b
Tatiana Churkina <sharonova_t@inbox.ru>
2023-08-17 13:32:24 +0300

    Убрала кнопку выключения музыки перед демо

--- packages/client/src/components/bomberman/index.tsx
+++ packages/client/src/components/bomberman/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useEffect, useRef, useState } from 'react'
 import FullscreenIcon from '@mui/icons-material/Fullscreen'
 import FullscreenExitIcon from '@mui/icons-material/FullscreenExit'
-import VolumeOffIcon from '@mui/icons-material/VolumeOff'
 import './bomberman.css'
 import { Button, Fab } from '@mui/material'
 import {
@@ -92,17 +91,6 @@ const Bomberman: FC = () => {
     // @ts-ignore
     audioRef.current.pause()
   }
-  const toggleMusic = () => {
-    if (audioRef.current) {
-      const audio: HTMLAudioElement = audioRef.current
-      if (audio && audio.paused && audio.readyState != 0) {
-        audio.play()
-      } else {
-        audio.pause()
-        audio.currentTime = 0
-      }
-    }
-  }
 
   const startGame = () => {
     if (bomber) {
@@ -148,13 +136,6 @@ const Bomberman: FC = () => {
       <div className="bomberman__buttons">
         <Button onClick={startGame}>Начать Игру</Button>
         <Button onClick={stopGame}>Окончить Игру</Button>
-        <Button
-          onClick={toggleMusic}
-          aria-label="mute music"
-          color="primary"
-          size="small">
-          <VolumeOffIcon />
-        </Button>
         <Fab
           onClick={toggleFullScreen}
           aria-label="full screen mode"
