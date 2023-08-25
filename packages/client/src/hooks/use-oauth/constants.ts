f48da1494470a644aaaf46c877ecf5d1dea3a6c0
Ivan Axenenko <ioaksenenko@gmail.com>
2023-08-25 10:28:29 +0700

    added redirect uri to env

--- packages/client/src/hooks/use-oauth/constants.ts
+++ packages/client/src/hooks/use-oauth/constants.ts
@@ -1,4 +1,4 @@
 export const YANDEX_LOGIN_URL = '/oauth/yandex'
 export const CLIENT_ID_RETRIEVE_URL = '/oauth/yandex/service-id'
 
-export const REDIRECT_URI = 'http://localhost:3000'
+export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI
