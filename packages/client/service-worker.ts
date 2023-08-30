e6b46a49da9813da24441580dc2cf4531785f50e
Ivan Axenenko <ioaksenenko@gmail.com>
2023-08-30 12:47:35 +0700

    fixed type bugs

--- packages/client/service-worker.ts
+++ packages/client/service-worker.ts
@@ -8,36 +8,36 @@ const URLS = ['/index.html']
 
 declare const self: ServiceWorkerGlobalScope
 
-const addCaches = async () => {
-  try {
-    const cache: Cache = await caches.open(`${version}${cacheName}`)
-
-    return cache.addAll(URLS)
-  } catch (error) {
-    console.error(error instanceof Error ? error.message : error)
-  }
-}
-
 self.addEventListener('install', event => {
-  event.waitUntil(addCaches)
+  event.waitUntil(
+    caches.open(
+      `${version}${cacheName}`
+    ).then(
+      cache => cache.addAll(URLS)
+    ).catch(
+      error => console.error(
+        error instanceof Error
+          ? error.message
+          : error
+      )
+    )
+  )
 })
 
-const checkCaches = async () => {
-  try {
-    const cacheNames = await caches.keys()
-
-    return await Promise.all(
-      cacheNames
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(
+      cacheNames => cacheNames
         .filter(name => name !== `${version}${cacheName}`)
         .map(name => caches.delete(name))
+    ).catch(
+      error => console.error(
+        error instanceof Error
+          ? error.message
+          : error
+      )
     )
-  } catch (error) {
-    console.error(error instanceof Error ? error.message : error)
-  }
-}
-
-self.addEventListener('activate', event => {
-  event.waitUntil(checkCaches)
+  )
 })
 
 const getFromNetwork = (
