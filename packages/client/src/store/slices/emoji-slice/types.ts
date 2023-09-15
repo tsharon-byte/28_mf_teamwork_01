e628bf01085546a6bea6d0b8e21330822c2bb787
Tatiana Churkina <sharonova_t@inbox.ru>
2023-09-15 16:43:33 +0300

    28-52: Добавлены emoji, комментарии после первого ревью

--- packages/client/src/store/slices/emoji-slice/types.ts
+++ packages/client/src/store/slices/emoji-slice/types.ts
@@ -2,7 +2,7 @@ import { Nullable } from '../../../types'
 import IError from '../../../helpers/prepare-error/types'
 
 export type EmojiInitialState = {
-  emojies: EmojiType[] | []
+  emojies: EmojiType[]
   loading: boolean
   error: Nullable<IError>
 }
