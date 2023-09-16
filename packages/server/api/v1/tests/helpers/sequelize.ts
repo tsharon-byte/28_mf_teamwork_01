3b8fae7e8cc406b7208066cb190bafc6a8928187
Ivan Axenenko <ioaksenenko@gmail.com>
2023-09-16 12:27:00 +0700

    fixed tests sequilize settings

--- packages/server/api/v1/tests/helpers/sequelize.ts
+++ packages/server/api/v1/tests/helpers/sequelize.ts
@@ -8,7 +8,7 @@ const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD } =
   process.env
 
 const sequelize = new Sequelize({
-  host: POSTGRES_HOST || 'postgres',
+  host: POSTGRES_HOST || 'localhost',
   port: Number(POSTGRES_PORT || '5432'),
   username: POSTGRES_USER || 'postgres',
   password: POSTGRES_PASSWORD || 'postgres',
