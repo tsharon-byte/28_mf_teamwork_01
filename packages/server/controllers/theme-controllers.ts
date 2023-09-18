743e74a9f1de6c7e2181b6f496279f188deea5b8
Sergey Kalachev <rollstream@yandex.ru>
2023-09-18 18:40:57 +0300

    controller fixed

--- packages/server/controllers/theme-controllers.ts
+++ packages/server/controllers/theme-controllers.ts
@@ -2,9 +2,10 @@ import type { Handler } from 'express'
 import { Theme } from '../db'
 
 export const getTheme: Handler = (_, res) => {
-  Theme.findAll({
-    attributes: ['id', 'mode'],
-  }).then(theme => {
-    res.status(200).send(theme)
+  Theme.findOne({
+    attributes: ['mode'],
+    order: [['id', 'DESC']],
+  }).then(mode => {
+    res.status(200).send(mode)
   })
 }
