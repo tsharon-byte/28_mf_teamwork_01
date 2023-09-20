c7ed9b0ac53c3f0a4770145d6a2a11062216116e
Sergey Kalachev <rollstream@yandex.ru>
2023-09-20 19:53:49 +0300

    colors fixed

--- packages/client/src/components/profile-components/profile-info/index.tsx
+++ packages/client/src/components/profile-components/profile-info/index.tsx
@@ -14,46 +14,48 @@ import styles from './styles.module.css'
 
 export const ProfileInfo: FC<ProfileInfoType> = memo(props => {
   const { user, handleOpenModal, mode, handleUpdateUserSubmit } = props
+  const mainColor = mode === 'light' ? '#000000' : '#FFFFFF'
+  const hoverColor = mode === 'light' ? '#322E2E' : '#FFD54F'
   return (
     <Form
       validator={updateUserValidator}
       onSubmit={handleUpdateUserSubmit}
       className={styles.form}>
       <EditTextField
-        mainColor={mode === 'light' ? '#000000' : ''}
-        hoverColor={mode === 'light' ? '#322E2E' : ''}
+        mainColor={mainColor}
+        hoverColor={hoverColor}
         value={user.login}
         name="login"
         label="Логин"
         validationRules={[loginValidationRule]}
       />
       <EditTextField
-        mainColor={mode === 'light' ? '#000000' : ''}
-        hoverColor={mode === 'light' ? '#322E2E' : ''}
+        mainColor={mainColor}
+        hoverColor={hoverColor}
         value={user.email}
         name="email"
         label="Email"
         validationRules={[emailValidationRule]}
       />
       <EditTextField
-        mainColor={mode === 'light' ? '#000000' : ''}
-        hoverColor={mode === 'light' ? '#322E2E' : ''}
+        mainColor={mainColor}
+        hoverColor={hoverColor}
         value={user.phone || ''}
         name="phone"
         label="Телефон"
         validationRules={[phoneValidationRule]}
       />
       <EditTextField
-        mainColor={mode === 'light' ? '#000000' : ''}
-        hoverColor={mode === 'light' ? '#322E2E' : ''}
+        mainColor={mainColor}
+        hoverColor={hoverColor}
         value={user.first_name}
         name="first_name"
         label="Имя"
         validationRules={[nameValidationRule]}
       />
       <EditTextField
-        mainColor={mode === 'light' ? '#000000' : ''}
-        hoverColor={mode === 'light' ? '#322E2E' : ''}
+        mainColor={mainColor}
+        hoverColor={hoverColor}
         value={user.second_name}
         name="second_name"
         label="Фамилия"
