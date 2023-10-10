9e5ebadc78fa6fce9617d189a7e7b23aefaadea6
Ivan Axenenko <ioaksenenko@gmail.com>
2023-10-10 16:14:26 +0700

    fixed speed

--- packages/client/src/game/enemies/kondoria/constants.ts
+++ packages/client/src/game/enemies/kondoria/constants.ts
@@ -19,6 +19,6 @@ export const DEAD_SPRITE = {
   ANIMATION_SPEED: 32,
 }
 
-export const SPEED = 5
+export const SPEED = 20
 export const POINTS_PER_KILL = 1000
 export const CAN_GO_THROUGH_WALLS = true
